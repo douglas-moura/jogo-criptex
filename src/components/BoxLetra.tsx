1f184e70e6b3772dc53f78f96a9b71ee59aacf77
Douglas Moura <douglas12345.dsm@gmail.com>
2025-12-06 12:59:25 -0300

    feat: Verificador de acertos de palpites implementado

--- src/components/BoxLetra.tsx
+++ src/components/BoxLetra.tsx
@@ -1,6 +1,20 @@
-import { View, Text, TextInput } from "react-native"
+import { View, Text, TextInput, NativeSyntheticEvent, TextInputChangeEventData } from "react-native"
+import { useState } from "react"
+
+function checarLetra(letraPalpite: string, letraCerta: string): boolean {
+    return letraPalpite.toLocaleUpperCase() == letraCerta.toLocaleUpperCase()
+}
 
 export default function BoxLetra({letra, caract}: {letra?: string, caract?: number}) {
+    const [value, setValue] = useState('')
+    const [acerto, setAcerto] = useState<boolean>(false)
+
+    const handleChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        const novoValor = e.nativeEvent.text
+        setValue(novoValor)
+        setAcerto(checarLetra(novoValor, letra || ''))
+    }
+
     return (
         <View style={{ flexDirection: 'column', alignItems: 'center', gap: 2, width: 28 }}>
             <View style={{ borderWidth: 0, borderColor: 'green', aspectRatio: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -12,7 +26,8 @@ export default function BoxLetra({letra, caract}: {letra?: string, caract?: numb
                     width: '100%',
                     padding: 0,
                     textAlign: 'center',
-                }} maxLength={1} />
+                    backgroundColor: value == '' ? 'white' : acerto ? 'lightgreen' : 'red'
+                }} maxLength={1} value={value} onChange={handleChange} editable={!acerto} />
             </View>
             <Text style={{
                 fontSize: 10,
